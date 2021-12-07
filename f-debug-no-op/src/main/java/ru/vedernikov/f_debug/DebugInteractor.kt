a2e1720c6796fab92900687637e462046adf2bb7
Viktor Vedernikov <Nupee^>
2021-12-07 16:28:26 +0500

    added f-debug-no-op

--- f-debug-no-op/src/main/java/ru/vedernikov/f_debug/DebugInteractor.kt
+++ f-debug-no-op/src/main/java/ru/vedernikov/f_debug/DebugInteractor.kt
@@ -1,13 +1,16 @@
 package ru.vedernikov.f_debug
 
+import okhttp3.OkHttpClient
+import ru.vedernikov.i_network.type.ServerType
+
 class DebugInteractor {
 
-    //var serverType: ServerType = ServerType.PROD
+    var serverType: ServerType = ServerType.PROD
 
     fun onCreateApp(icon: Int) { /* no action */
     }
 
     fun mustNotInitializeApp() = false
 
-    //fun configureOkHttp(builder: OkHttpClient.Builder) { /* no action */ }
+    fun configureOkHttp(builder: OkHttpClient.Builder) { /* no action */ }
 }
